c5ccaae7a49b2dba42bee930be9a89a97dc2f9fa
Oleksandr Butrym <butrym@karazin.ua>
2020-05-17 23:04:13 +0300

    Initial commit

---
+++ FL13_HW12/homework/app.js
@@ -0,0 +1,6 @@
+const root = document.getElementById('root');
+
+const bookList = document.createElement('div');
+bookList.classList.add('book-list');
+
+root.appendChild();
---
+++ FL13_HW12/homework/books.js
@@ -0,0 +1,42 @@
+const booksList = [
+    {
+        id: 0,
+        title: 'The Adventures of Tom Sawyer',
+        author: 'Mark Twain',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1d/Tom_Sawyer_1876_frontispiece.jpg',
+        plot: `Tom Sawyer, an orphan, lives with his Aunt Polly and his half-brother Sid in the fictional
+         town of St. Petersburg, Missouri sometime in the 1840s. A fun-loving boy, Tom skips school to go
+         swimming and is made to whitewash his aunt's fence for the entirety of the next day, Saturday,
+          as punishment...`
+    },
+    {
+        id: 1,
+        title: 'Adventures of Huckleberry Finn',
+        author: 'Mark Twain',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Huckleberry-finn-with-rabbit.jpg',
+        plot: `The story begins in fictional St. Petersburg, Missouri (based on the actual town of Hannibal,
+         Missouri), on the shore of the Mississippi River "forty to fifty years ago" (the novel having been
+         published in 1884). Huckleberry "Huck" Finn (the protagonist and first-person narrator) and his friend,
+         Thomas "Tom" Sawyer, have each come into a considerable sum of money as a result of their earlier adventures
+         (detailed in The Adventures of Tom Sawyer)...`
+    },
+    {
+        id: 2,
+        title: 'Alice\'s Adventures in Wonderland',
+        author: 'Lewis Carroll',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Alice_par_John_Tenniel_04.png',
+        plot: `Alice, a seven-year-old girl, is feeling bored and drowsy while sitting on the riverbank with her
+         elder sister. She notices a talking, clothed white rabbit with a pocket watch run past...`
+    },
+    {
+        id: 3,
+        title: 'Through the Looking-Glass',
+        author: 'Lewis Carroll',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Alice_knight.jpg',
+        plot: `Alice is playing with a white kitten (whom she calls "Snowdrop") and a black kitten
+         (whom she calls "Kitty") when she ponders what the world is like on the other side of
+         a mirror's reflection. Climbing up onto the fireplace mantel, she pokes at the wall-hung mirror
+         behind the fireplace and discovers, to her surprise, that she is able to step through it to
+         an alternative world...`
+    }
+];
